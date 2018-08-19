d412d172c9be791a18fb5e5c2fb1cec4c3b00ff1
Todor Dakov <tdd@dekom.bg>
2018-08-20 00:23:49 +0300

    Identity module refacture

---
+++ Source/Data/ForumSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using ForumSystem.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ForumSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- Source/Web/ForumSystem.Web/App_Start/IdentityConfig.cs
+++ Source/Web/ForumSystem.Web/App_Start/IdentityConfig.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using ForumSystem.Web.Models;
+using ForumSystem.Models;
+using ForumSystem.Data;
 
 namespace ForumSystem.Web
 {
