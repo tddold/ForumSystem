f156adf72fa83f0a54495f6621cb5bb7911019d4
Todor Dakov <tdd@dekom.bg>
2018-08-31 19:29:19 +0300

    Installed TinyMce

--- Source/Web/ForumSystem.Web/InputModels/Questions/AskInputModel.cs
+++ Source/Web/ForumSystem.Web/InputModels/Questions/AskInputModel.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ForumSystem.Web.InputModels.Questions
 {
     public class AskInputModel
     {
+        [Display(Name ="Title")]
+        public string Title { get; set; }
+
+        [AllowHtml]
+        [Display(Name = "Content")]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        [Display(Name = "Tag")]
+        public string Tags { get; set; }
     }
 }
