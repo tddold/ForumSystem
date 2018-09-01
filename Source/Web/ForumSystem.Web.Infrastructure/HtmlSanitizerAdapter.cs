8986f2924e69248fab7d14fc058c43f01c68c3eb
Todor Dakov <tdd@dekom.bg>
2018-09-01 17:48:55 +0300

    add all sanitizer

---
+++ Source/Web/ForumSystem.Web.Infrastructure/HtmlSanitizerAdapter.cs
@@ -0,0 +1,20 @@
+﻿using Ganss.XSS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSystem.Web.Infrastructure
+{
+    public class HtmlSanitizerAdapter : ISanitizer
+    {
+        public string Sanitize(string html)
+        {
+            var sanitizer = new HtmlSanitizer();
+            var result = sanitizer.Sanitize(html);
+
+            return result;
+        }
+    }
+}
---
+++ Source/Web/ForumSystem.Web/ViewModels/Questions/QuestionDisplayViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForumSystem.Web.ViewModels.Questions
+{
+    public class QuestionDisplayViewModel
+    {
+        public string Title { get; set; }
+
+        public string Contet { get; set; }
+    }
+}
