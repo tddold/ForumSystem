c5b9a53778bf649df801a7ef8c1f316d607f6d63
tddold <tdd@dekom.bg>
2018-09-03 02:54:00 +0300

    Add Kendo Libraries

--- Source/Web/ForumSystem.Web/App_Start/BundleConfig.cs
+++ Source/Web/ForumSystem.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Optimization;
 
 namespace ForumSystem.Web
@@ -8,18 +9,38 @@ namespace ForumSystem.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
+            RegisterScriptBundles(bundles);
+            RegisterStyleBundles(bundles);
+        }
+
+        private static void RegisterStyleBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                        "~/Content/kendo/kendo.common-bootstrap.min.css",
+                        "~/Content/kendo/kendo.silver.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                       "~/Content/bootstrap.cosmo.css",
+                       "~/Content/site.css"));
+        }
+
+        private static void RegisterScriptBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                    "~/Scripts/kendo/kendo.all.min.js",
+                    "~/Script/kendo/kendo.aspnetmvc.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                    "~/Scripts/kendo/jquery.min.js"));
+                    //"~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.cosmo.css",
-                      "~/Content/site.css"));
         }
     }
 }
